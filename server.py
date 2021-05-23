28628307ead7af01c743c4b37da3774a83edabaf
Igor Kozlinskyi <ihor.kozlinskyi.dev@gmail.com>
2021-05-23 10:45:32 +0300

    save raw data remove unused code

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from urllib.parse import urlparse, parse_qs
 from pathlib import Path
 from src import heatmap_transform
 import numpy as np
+import re
+from datetime import datetime
 
 hostName = "localhost"
 serverPort = 8080
@@ -12,14 +14,22 @@ def exp_data_to_float_arr(exp_data_arr):
     res = [x[:-1].split(",") for x in exp_data_arr]
     return np.float64(res)
 
+def get_valid_filename(s):
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
+
+def save_raw_data(raw_data, filename):
+    with open("./data/raw/%s.txt" % filename, 'w') as f:
+        f.write(raw_data)
 
 def process_data(raw_data: str):
     lines = np.array(raw_data.strip().split("\n"))
     group_by_experiment = lines.reshape(int(len(lines) / 9), 9)
+    save_raw_data(raw_data, get_valid_filename(datetime.now()))
     for experiment in group_by_experiment:
         exp_timestamp = experiment[0]
         exp_data = exp_data_to_float_arr(experiment[1:])
-        filename = exp_timestamp.strip().replace("[\\/:*?\"<>|]", "").replace(" ", "_")
+        filename = get_valid_filename(exp_timestamp)
         heatmap_transform.save_heatmap(exp_data, filename)
 
 
--- src/heatmap_transform.py
+++ src/heatmap_transform.py
@@ -3,24 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 from scipy.interpolate import interp2d
-import re
 
 NORMALIZATION = Normalize(15, 80)
 
-data = np.array([
-    [27.00, 28.25, 29.25, 28.75, 27.75, 26.50, 25.25, 24.00],
-    [27.25, 28.00, 29.75, 30.25, 28.00, 26.00, 24.50, 22.75],
-    [26.00, 27.25, 28.50, 29.50, 28.75, 27.00, 24.00, 24.00],
-    [25.50, 27.00, 28.00, 29.00, 29.25, 29.25, 25.75, 23.75],
-    [25.50, 26.25, 27.00, 28.75, 28.75, 28.75, 29.00, 25.00],
-    [25.25, 25.25, 26.50, 27.75, 28.75, 29.00, 29.00, 27.25],
-    [25.00, 25.25, 26.25, 27.00, 28.25, 29.00, 29.50, 29.50],
-    [24.25, 24.75, 25.75, 26.50, 28.00, 28.00, 29.25, 28.25]
-])
-
-def get_valid_filename(s):
-    s = str(s).strip().replace(' ', '_')
-    return re.sub(r'(?u)[^-\w.]', '', s)
 
 def save_heatmap(data, name):
     x = np.arange(0, 8)
@@ -36,7 +21,6 @@ def save_heatmap(data, name):
                      norm=NORMALIZATION)
 
     plt.colorbar(ScalarMappable(norm=NORMALIZATION, cmap='plasma'))
-    
-    valid_name = get_valid_filename(name)
-    plt.savefig('data/processed/%s.png' % valid_name)
+
+    plt.savefig('./data/processed/%s.png' % name)
     plt.clf()
