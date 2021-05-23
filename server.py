904f3309f9cc8938fe94733b0af9f42c5555eac7
Igor Kozlinskyi <ihor.kozlinskyi.dev@gmail.com>
2021-05-23 23:13:32 +0300

    fix

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def get_valid_filename(s):
 
 def save_raw_data(date, raw_data, filename):
     with open("./data/raw/data.txt", 'a') as f:
-        f.write(date)
+        f.write(str(date))
         f.write(raw_data)
 
 def process_data(raw_data: str):
